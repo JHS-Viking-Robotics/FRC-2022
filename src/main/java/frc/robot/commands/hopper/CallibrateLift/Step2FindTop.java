7d0fbfb4207d3a30a8142c4b6a6d40532b4217c2
Dan Erickson <daniel@dans-skynet.com>
2021-08-07 18:01:52 -0400

    Add Hopper Lift calibration command
    When the robot boots up, the Hopper Lift does not always start in the same position nor does the encoder always return the right value. Additionally, if the encoder is jostled, the values can change.
    
    Added a sequential command which runs the motor up and down to find the limits of its movement, and then resets the setpoint values for the Lift. This still needs testing.
    
    See Also:
    - #27

---
+++ src/main/java/frc/robot/commands/hopper/CallibrateLift/Step2FindTop.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.hopper.CallibrateLift;
+
+import frc.robot.subsystems.Hopper;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Step2FindTop extends CommandBase {
+
+  private final Hopper hopper;
+  
+  /** Creates a new Step2FindTop. */
+  public Step2FindTop(Hopper subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.hopper = subsystem;
+    addRequirements(this.hopper);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // The Lift.BOTTOM position should now be set, and the approximate range of
+    // motion is about 890 ticks. Therefore the Lift should have around 2.5
+    // seconds to reach 600 ticks. If this is a problem, step 1.5 can be made
+    // which waits for the Lift to get to 850, then we can run this step
+    hopper.setLiftSetpointTicks(600);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // Increment the Lift up until the error is more than double our increment
+    hopper.setLiftSetpointTicks(
+        hopper.getLiftSetpointTicks() + 2);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    // Reset the Lift up setpoint to the new position on the sensor
+    if (!interrupted) {
+      hopper.resetLiftSetpoint(
+        Hopper.Lift.UP,
+        hopper.getLiftSetpointTicks());
+    }
+    hopper.setAllNeutral();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    // Keep going up until the error gets higher than our increment
+    return hopper.getLiftPositionError() > 4;
+  }
+}
