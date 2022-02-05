14aa58d69f72b3432a0dbbf322c2215973642731
Dan Erickson <daniel@dans-skynet.com>
2022-02-05 16:13:46 -0500

    Import auton code from feature/basic-auton

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +15,130 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+  /** Math and conversion constants to 6 significant figures */
+  public final static class Math {
+    /** The mathematical constant pi */
+    public final static double PI = 3.14159;
+    /** Multiply to get from inches to meters */
+    public final static double INCHES_2_METERS = 0.0254000;
+    /** Multiply to get from meters to inches */
+    public final static double METERS_2_INCHES = 39.3701;
+  }
+
+  /** Configuration options, PID constants, and CAN Bus ID's for various subsystems */
+  public final static class Subsystem {
+    /** Configuration options, PID constants, and CAN Bus ID's for {@link frc.robot.subsystems.Drivetrain Drivetrain} subsystem */
+    public final static class Drivetrain {
+      /** CAN Bus ID of left front drive SparkMAX */
+      public final static int LEFT_FRONT_ID = 3;
+      /** CAN Bus ID of left back drive SparkMAX */
+      public final static int LEFT_BACK_ID = 53;
+      /** CAN Bus ID of right front drive SparkMAX */
+      public final static int RIGHT_FRONT_ID = 1;
+      /** CAN Bus ID of right back drive SparkMAX */
+      public final static int RIGHT_BACK_ID = 51;
+      /** Left front SparkMAX is inverted */
+      public final static boolean LEFT_FRONT_INVERTED = false;
+      /** Left back SparkMAX is inverted */
+      public final static boolean LEFT_BACK_INVERTED = true;
+      /** Right front SparkMAX is inverted */
+      public final static boolean RIGHT_FRONT_INVERTED = false;
+      /** Right back SparkMAX is inverted */
+      public final static boolean RIGHT_BACK_INVERTED = true;
+      /** P constant for SparkMAX onboard PID control */
+      public final static double P = 0.0;
+      /** I constant for SparkMAX onboard PID control */
+      public final static double I = 0.0;
+      /** D constant for SparkMAX onboard PID control */
+      public final static double D = 0.0;
+      /** F constant for SparkMAX onboard PID control */
+      public final static double F = 0.0;
+      /** Maximum forward driving velocity in m/s (meters per second). Should
+       * be slightly lower than robot's maximum free speed */
+      public final static double MAX_VELOCITY = 2.0;
+      /** Maximum forward driving acceleration in m/s^2 (meters per second
+       * squared) */
+      public final static double MAX_ACCELERATION = 2.0;
+      /** Maximum rotational velocity in rad/s */
+      public final static double MAX_ROTATION = Math.PI/4.0;
+      /** kS constant with units V (volts) for Drivetrain motor
+       * characterization. See
+       * <a href=https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-characterization/introduction.html">FRC Docs</a>
+       * section on motor characterization for more */
+      public final static double S = 1.48;
+      /** kV constant with units (Vs)/m (volt seconds per meter) for Drivetrain
+       * motor characterization. See
+       * <a href=https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-characterization/introduction.html">FRC Docs</a>
+       * section on motor characterization for more */
+      public final static double V = 2.93;
+      /** kA constant with units (Vs^2)/m (volt seconds squared per meter) for
+       * Drivetrain motor characterization. See
+       * <a href=https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-characterization/introduction.html">FRC Docs</a>
+       * section on motor characterization for more */
+      public final static double A = 0.0313;
+      /** MecanumDriveKinematics object for the Drivetrain */
+      public final static MecanumDriveKinematics KINEMATICS = new MecanumDriveKinematics(
+          new Translation2d(0.381, 0.381),
+          new Translation2d(0.381, -0.381),
+          new Translation2d(-0.381, 0.381),
+          new Translation2d(-0.381, -0.381));
+    }
+
+    /** Configuration options, PID constants, and CAN Bus ID's for {@link frc.robot.subsystems.Hopper Hopper} subsystem */
+    public final class Hopper {
+    /** CAN Bus ID of lift Talon controller */
+    public final static int LIFT_ID = 2;
+    /** CAN Bus ID of intake Victor controller */
+    public final static int INTAKE_ID = 54;
+    /** Lift Talon controller is inverted */
+    public final static boolean LIFT_CONTROLLER_INVERTED = true;
+    /** Lift Talon sensor is inverted */
+    public final static boolean LIFT_SENSOR_INVERTED = false;
+    /** Intake Victor is inverted */
+    public final static boolean INTAKE_CONTROLLER_INVERTED = true;
+    /** P constant for Lift Talon onboard PID control */
+    public final static double LIFT_P = 1.0;
+    /** I constant for Lift Talon onboard PID control */
+    public final static double LIFT_I = 0.0;
+    /** D constant for Lift Talon onboard PID control */
+    public final static double LIFT_D = 0.0;
+    /** F constant for Lift Talon onboard PID control */
+    public final static double LIFT_F = 0.0;
+    /** Tick position for Lift Up setpoint */
+    public final static double LIFT_UP = 890.0;
+    /** Tick position for Lift Dispense setpoint */
+    public final static double LIFT_DISPENSE = 728.0;
+    /** Tick position for Lift Down setpoint */
+    public final static double LIFT_DOWN = 0.0;
+    /** Percent output for Intake In mode */
+    public final static double INTAKE_IN = 0.5;
+    /** Percent output for Intake Out mode */
+    public final static double INTAKE_OUT = 0.5;
+    /** Current output for Intake Hold mode */
+    public final static double INTAKE_HOLD = 0.5;
+    }
+  }
+  
+  /** Joystick USB ports */
+  public final class Joystick {
+    /** USB port for driver controller */
+    public final static int DRIVER = 0;
+    /** USB port for support controller */
+    public final static int SUPPORT = 1;
+  }
+  
+  /** Physical chassis measurements and dimensions */
+  public final class Chassis {
+    /** Wheel diameter in meters */
+    public final static double WHEEL_DIAMETER = 6 * Math.INCHES_2_METERS;
+    /** Wheel circumference in meters */
+    public final static double WHEEL_CIRCUM = WHEEL_DIAMETER * Math.PI;
+    /** Width between two wheels (axle length) in meters */
+    public final static double TRACK_WIDTH = 23 * Math.INCHES_2_METERS;
+    /** Width of the robot in meters */
+    public final static double WIDTH = 27 * Math.INCHES_2_METERS;
+    /** Length of the robot in meters */
+    public final static double LENGTH = 32.3 * Math.INCHES_2_METERS;
+  }
+  }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,10 +4,28 @@
 
 package frc.robot.subsystems;
 
+import java.util.Map;
+
+import frc.robot.Constants;
+import frc.robot.Constants.Subsystem;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
@@ -16,21 +34,275 @@ public class Drivetrain extends SubsystemBase {
   private final CANSparkMax leftBack;       // Left side rear motor
   private final CANSparkMax rightFront;     // Right side front motor
   private final CANSparkMax rightBack;      // Right side rear motor
+  private final RelativeEncoder leftFrontEncoder;  // Left side front encoder
+  private final RelativeEncoder leftBackEncoder;   // Left side rear encoder
+  private final RelativeEncoder rightFrontEncoder; // Right side front encoder
+  private final RelativeEncoder rightBackEncoder;  // Right side rear encoder
   private final MecanumDrive driveMecanum;  // Mecanum drive interface
   private final ADXRS450_Gyro driveGyro;    // Gyroscope for determining robot heading
+  private final MecanumDriveOdometry driveOdometry; // Odometry object for keeping track of robot Pose
+
+  private NetworkTableEntry leftDistance;   // NetworkTables odometer for left side sensors
+  private NetworkTableEntry rightDistance;  // NetworkTables odometer for right side sensors
+  private NetworkTableEntry leftVelocity;   // NetworkTables speedometer for left side sensors
+  private NetworkTableEntry rightVelocity;  // NetworkTables speedometer for right side sensors
+
+  private NetworkTableEntry driveP;         // kP for Talon closed-loop PID
+  private NetworkTableEntry driveI;         // kI for Talon closed-loop PID
+  private NetworkTableEntry driveD;         // kD for Talon closed-loop PID
+  private NetworkTableEntry driveF;         // kF for Talon closed-loop PID
+  /** Hopper Intake modes of operation */
+  public enum Motors {
+    /** Left Front drive motor */
+    LEFT_FRONT,
+    /** Left Back drive motor */
+    LEFT_BACK,
+    /** Right Front drive motor */
+    RIGHT_FRONT,
+    /** Right Back drive motor */
+    RIGHT_BACK,
+    /** Both Right side drive motors */
+    RIGHT,
+    /** Both Left side drive motors */
+    LEFT,
+    /** All drive motors together */
+    ALL
+  }
 
   private final double driveSpeedScalar = 0.6; // Percent of max output for motors
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
-    leftFront = new CANSparkMax(1, MotorType.kBrushless);
-    rightFront = new CANSparkMax(2, MotorType.kBrushless);
-    leftBack = new CANSparkMax(3, MotorType.kBrushless);
-    rightBack = new CANSparkMax(4, MotorType.kBrushless);
+    leftFront = new CANSparkMax(Subsystem.Drivetrain.LEFT_FRONT_ID, MotorType.kBrushless);
+    rightFront = new CANSparkMax(Subsystem.Drivetrain.LEFT_BACK_ID, MotorType.kBrushless);
+    leftBack = new CANSparkMax(Subsystem.Drivetrain.RIGHT_FRONT_ID, MotorType.kBrushless);
+    rightBack = new CANSparkMax(Subsystem.Drivetrain.RIGHT_BACK_ID, MotorType.kBrushless);
+
+    leftFront.restoreFactoryDefaults();
+    leftBack.restoreFactoryDefaults();
+    rightFront.restoreFactoryDefaults();
+    rightBack.restoreFactoryDefaults();
+
+    leftFront.setInverted(Subsystem.Drivetrain.LEFT_FRONT_INVERTED);
+    leftBack.setInverted(Subsystem.Drivetrain.LEFT_BACK_INVERTED);
+    rightFront.setInverted(Subsystem.Drivetrain.RIGHT_FRONT_INVERTED);
+    rightBack.setInverted(Subsystem.Drivetrain.RIGHT_BACK_INVERTED);
+
+    leftFrontEncoder = leftFront.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
+    leftBackEncoder = leftBack.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
+    rightFrontEncoder = rightFront.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
+    rightBackEncoder = rightBack.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
+    leftFrontEncoder.setPositionConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
+    leftBackEncoder.setPositionConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
+    rightFrontEncoder.setPositionConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
+    rightBackEncoder.setPositionConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
+    leftFrontEncoder.setVelocityConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
+    leftBackEncoder.setVelocityConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
+    rightFrontEncoder.setVelocityConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
+    rightBackEncoder.setVelocityConversionFactor(
+        (10.71 * 42.0) / Constants.Chassis.WHEEL_DIAMETER);
 
     driveMecanum = new MecanumDrive(leftFront, leftBack, rightFront, rightBack);
     driveGyro = new ADXRS450_Gyro();
     driveGyro.reset();
+    driveOdometry = new MecanumDriveOdometry(Subsystem.Drivetrain.KINEMATICS, getGyroRotation());
+
+    configureShuffleboard();
+  }
+
+  /** Configures the Shuffleboard dashboard "Drivetrain" tab */
+  private void configureShuffleboard() {
+    // Create references to Drivetrain tab and its various layouts
+    ShuffleboardTab shuffleDrivetrainTab = Shuffleboard.getTab("Drivetrain");
+    ShuffleboardLayout shufflePIDLayout = shuffleDrivetrainTab
+        .getLayout("PID", BuiltInLayouts.kList)
+        .withProperties(Map.of("Label position", "LEFT"))
+        .withPosition(5, 0)
+        .withSize(1, 4);
+    ShuffleboardLayout shuffleDistanceLayout = shuffleDrivetrainTab
+        .getLayout("Odometer (m)", BuiltInLayouts.kGrid)
+        .withProperties(Map.of(
+            "Label position",    "BOTTOM",
+            "Number of columns", 2,
+            "Number of rows",    1))
+        .withPosition(0, 0)
+        .withSize(2, 1);
+    // Note that NetworkTables uses a flat namespace where the keys use '/' to
+    // denote hierarchy, so we manually set the title in Shuffleboard instead
+    ShuffleboardLayout shuffleVelocityLayout = shuffleDrivetrainTab
+        .getLayout("Speedometer (ms)", BuiltInLayouts.kGrid)
+        .withProperties(Map.of(
+            "Label position",    "BOTTOM",
+            "Number of columns", 2,
+            "Number of rows",    1))
+        .withPosition(0, 1)
+        .withSize(2, 1);
+
+    // Add drivetrain objects to tab
+    shuffleDrivetrainTab
+        .add("Mecanum Drivetrain", driveMecanum)
+        .withPosition(2, 0)
+        .withSize(3, 4);
+    shuffleDrivetrainTab
+        .add("Gyro Angle (degrees)", driveGyro)
+        .withPosition(0,2)
+        .withSize(2,2);
+
+    // Configure PID list widget, and set default values from Constants
+    driveP = shufflePIDLayout
+        .add("P", Subsystem.Drivetrain.P)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+    driveI = shufflePIDLayout
+        .add("I", Subsystem.Drivetrain.I)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+    driveD = shufflePIDLayout
+        .add("D", Subsystem.Drivetrain.D)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+    driveF = shufflePIDLayout
+        .add("F", Subsystem.Drivetrain.F)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+
+    // Set up the encoder indicators
+    leftDistance = shuffleDistanceLayout
+        .add("Left", 0)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+    rightDistance = shuffleDistanceLayout
+        .add("Right", 0)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+    leftVelocity = shuffleVelocityLayout
+        .add("Left", 0)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+    rightVelocity = shuffleVelocityLayout
+        .add("Right", 0)
+        .withWidget(BuiltInWidgets.kTextView)
+        .getEntry();
+  }
+
+  /** Get the left encoder total distance travelled in meters */
+  public double getDistance(Motors motor) {
+    switch (motor) {
+      case LEFT_FRONT:
+        return leftFrontEncoder.getPosition();
+      case LEFT_BACK:
+        return leftBackEncoder.getPosition();
+      case RIGHT_FRONT:
+        return rightFrontEncoder.getPosition();
+      case RIGHT_BACK:
+        return rightBackEncoder.getPosition();
+      case LEFT:
+        return (getDistance(Motors.LEFT_FRONT) + getDistance(Motors.LEFT_BACK)) / 2.0;
+      case RIGHT:
+        return (getDistance(Motors.RIGHT_FRONT) + getDistance(Motors.RIGHT_BACK)) / 2.0;
+      case ALL:
+        return (getDistance(Motors.LEFT) + getDistance(Motors.RIGHT)) / 2.0;
+      default:
+        throw new IllegalArgumentException("Illegal arg to Drivetrain.getDiscance(Motors)");
+    }
+  }
+
+  /** Get the left encoder velocity in meters per second */
+  public double getVelocity(Motors motor) {
+    switch (motor) {
+      case LEFT_FRONT:
+        return leftFrontEncoder.getVelocity();
+      case LEFT_BACK:
+        return leftBackEncoder.getVelocity();
+      case RIGHT_FRONT:
+        return rightFrontEncoder.getVelocity();
+      case RIGHT_BACK:
+        return rightBackEncoder.getVelocity();
+      case LEFT:
+        return (getVelocity(Motors.LEFT_FRONT) + getVelocity(Motors.LEFT_BACK)) / 2.0;
+      case RIGHT:
+        return (getVelocity(Motors.RIGHT_FRONT) + getVelocity(Motors.RIGHT_BACK)) / 2.0;
+      case ALL:
+        return (getVelocity(Motors.LEFT) + getVelocity(Motors.RIGHT)) / 2.0;
+      default:
+        throw new IllegalArgumentException("Illegal arg to Drivetrain.getDiscance(Motors)");
+    }
+  }
+
+  /** Get the current WheelSpeeds of the robot */
+  public MecanumDriveWheelSpeeds getWheelSpeeds() {
+    return new MecanumDriveWheelSpeeds(
+        getVelocity(Motors.LEFT_FRONT),
+        getVelocity(Motors.RIGHT_FRONT),
+        getVelocity(Motors.LEFT_BACK),
+        getVelocity(Motors.RIGHT_BACK));
+  }
+
+  /** Reset the encoders to zero */
+  public void resetEncoders() {
+    resetEncoders(0);
+  }
+
+  /** Reset the encoders to the position */
+  public void resetEncoders(double position) {
+    leftFront.getEncoder().setPosition(position);
+    leftBack.getEncoder().setPosition(position);
+    rightFront.getEncoder().setPosition(position);
+    rightBack.getEncoder().setPosition(position);
+  }
+
+  /** Get the current rotational heading from the Gyroscope in degrees [-180, 180] */
+  public double getGyroAngle() {
+    return driveGyro.getRotation2d().getDegrees();
+  }
+
+  /** Get the current rotational velocity in degrees per second */
+  public double getGyroVelocity() {
+    return driveGyro.getRate();
+  }
+
+  /** Get the current rotational heading from the Gyroscope as a Rotation2d */
+  public Rotation2d getGyroRotation() {
+    return driveGyro.getRotation2d();
+  }
+
+  /** Reset the Gyroscope heading to 0. Note that this won't work correctly if
+   * the robot is moving, and the subsystem will refuse to perform the reset
+   * while moving
+   */
+  public void resetGyro() {
+    if (getVelocity(Motors.LEFT) != 0 || getVelocity(Motors.RIGHT) != 0) {
+      System.out.println("WARNING: Do not try to reset the gyroscope while the robot is moving");
+      return;
+    }
+    driveGyro.reset();
+  }
+
+  /** Return the current estimated Pose2d of the robot */
+  public Pose2d getPose() {
+    return driveOdometry.getPoseMeters();
+  }
+
+  /** Reset the odometry model to the specified Pose2d */
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    driveOdometry.resetPosition(pose, getGyroRotation());
+  }
+
+  /**
+   * Reset the odometry model and the encoders
+   *
+   * @see #resetOdometry(Pose2d)
+   */
+  public void resetOdometry() {
+    resetOdometry(getPose());
   }
 
   /** Drives the robot using ArcadeDrive style control */
@@ -50,13 +322,47 @@ public class Drivetrain extends SubsystemBase {
         driveSpeedScalar * slide, driveSpeedScalar * rotation, getGyroAngle());
   }
 
-  /** Get the angle of the gyroscope */
-  public double getGyroAngle() {
-    return driveGyro.getRotation2d().getDegrees();
+  /** Tank drive using voltage output to the motors */
+  public void tankDriveVoltage(double left, double right) {
+    leftFront.setVoltage(left);
+    leftBack.setVoltage(left);
+    rightFront.setVoltage(right);
+    rightBack.setVoltage(right);
+  }
+
+  /**
+   * Update the PIDF configuration for both encoders from the NetworkTables values
+   * configured on the Shuffleboard
+   */
+  public void syncNetworkTables() {
+    // Push Drivetrain PIDF constants from NetworkTables to the controllers
+    leftFront.getPIDController().setP(driveP.getDouble(Subsystem.Drivetrain.P));
+    leftFront.getPIDController().setI(driveI.getDouble(Subsystem.Drivetrain.I));
+    leftFront.getPIDController().setD(driveD.getDouble(Subsystem.Drivetrain.D));
+    leftFront.getPIDController().setFF(driveF.getDouble(Subsystem.Drivetrain.F));
+    leftBack.getPIDController().setP(driveP.getDouble(Subsystem.Drivetrain.P));
+    leftBack.getPIDController().setI(driveI.getDouble(Subsystem.Drivetrain.I));
+    leftBack.getPIDController().setD(driveD.getDouble(Subsystem.Drivetrain.D));
+    leftBack.getPIDController().setFF(driveF.getDouble(Subsystem.Drivetrain.F));
+    rightFront.getPIDController().setP(driveP.getDouble(Subsystem.Drivetrain.P));
+    rightFront.getPIDController().setI(driveI.getDouble(Subsystem.Drivetrain.I));
+    rightFront.getPIDController().setD(driveD.getDouble(Subsystem.Drivetrain.D));
+    rightFront.getPIDController().setFF(driveF.getDouble(Subsystem.Drivetrain.F));
+    rightBack.getPIDController().setP(driveP.getDouble(Subsystem.Drivetrain.P));
+    rightBack.getPIDController().setI(driveI.getDouble(Subsystem.Drivetrain.I));
+    rightBack.getPIDController().setD(driveD.getDouble(Subsystem.Drivetrain.D));
+    rightBack.getPIDController().setFF(driveF.getDouble(Subsystem.Drivetrain.F));
+
+    // Push the current potition data from the sensors to the NetworkTables
+    leftDistance.setDouble(getDistance(Motors.LEFT));
+    rightDistance.setDouble(getDistance(Motors.RIGHT));
+    leftVelocity.setDouble(getVelocity(Motors.LEFT));
+    rightVelocity.setDouble(getVelocity(Motors.RIGHT));
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    syncNetworkTables();
+    driveOdometry.update(getGyroRotation(), getWheelSpeeds());
   }
 }
